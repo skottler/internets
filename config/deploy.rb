a5ee1d7e2686fd0d66b62c2580db169437dfb158
Sam Kottler <sam@kottlerdevelopment.com>
2013-04-28 22:35:25 -0400

    A deployment process and about page

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,3 +5,11 @@ set :deploy_to, "/var/apps/#{application}"
 set :user, ENV["USER"]
 
 role :web, ENV["webserver"]
+
+after "deploy", "deploy:jekyll"
+
+namespace :deploy do
+  task :jekyll do
+    run("cd #{deploy_to}/current && bundle install && jekyll")
+  end
+end
